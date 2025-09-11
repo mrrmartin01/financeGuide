54e4a653999a437f9e7ce5c3b7456fecf231d707
mrmartin <theophilusmartin123@gmail.com>
2025-09-11 13:27:29 -0500

    Create Db context

--- Data/FinanceAppContext.cs
+++ Data/FinanceAppContext.cs
@@ -1,7 +1,12 @@
+using financeGuide.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace financeGuide.Data
 {
-    public class FinanceAppContext
+    public class FinanceAppContext : DbContext
     {
-        
+        public FinanceAppContext(DbContextOptions<FinanceAppContext> options) : base(options) {}
+
+        DbSet<Expense> Expenses { get; set; }
     }
 }
