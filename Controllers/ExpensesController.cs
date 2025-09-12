0c969aff72cd3e60a53535d5172527584ad070e3
mrmartin <theophilusmartin123@gmail.com>
2025-09-12 13:40:19 -0500

    Implement async Post and Get action

---
+++ Controllers/ExpensesController.cs
@@ -0,0 +1,35 @@
+﻿using financeGuide.Data;
+using financeGuide.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace financeGuide.Controllers
+{
+    public class ExpensesController(FinanceAppContext context) : Controller
+    {
+        private readonly FinanceAppContext _context = context;
+
+        public async Task<IActionResult> Index()
+        {
+            var expenses = await _context.Expenses.ToListAsync();
+            return View(expenses);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Expense expense)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Expenses.Add(expense);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
